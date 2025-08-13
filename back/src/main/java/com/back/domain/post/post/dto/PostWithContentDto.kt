537365dba499fe1454b2850b3bc8e67fc390df5f
jhs512 <jangka512@gmail.com>
2025-08-13 12:46:19 +0900

    0013

--- back/src/main/java/com/back/domain/post/post/dto/PostWithContentDto.kt
+++ back/src/main/java/com/back/domain/post/post/dto/PostWithContentDto.kt
@@ -3,21 +3,14 @@ package com.back.domain.post.post.dto
 import com.back.domain.post.post.entity.Post
 import java.time.LocalDateTime
 
-class PostWithContentDto private constructor(
-    id: Int,
-    createDate: LocalDateTime,
-    modifyDate: LocalDateTime,
-    authorId: Int,
-    authorName: String,
-    title: String,
+data class PostWithContentDto(
+    val id: Int,
+    val createDate: LocalDateTime,
+    val modifyDate: LocalDateTime,
+    val authorId: Int,
+    val authorName: String,
+    val title: String,
     val content: String
-) : PostDto(
-    id,
-    createDate,
-    modifyDate,
-    authorId,
-    authorName,
-    title
 ) {
     constructor(post: Post) : this(
         id = post.id,
