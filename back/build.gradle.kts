97a87497a4be1d9a4cbe0137fa9f7905e49010d9
jhs512 <jangka512@gmail.com>
2025-08-15 23:54:32 +0900

    0048

--- back/build.gradle.kts
+++ back/build.gradle.kts
@@ -37,9 +37,6 @@ dependencies {
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 
-    compileOnly("org.projectlombok:lombok")
-    annotationProcessor("org.projectlombok:lombok")
-
     implementation("org.springframework.boot:spring-boot-starter-validation")
     testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
 
